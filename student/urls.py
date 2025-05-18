13839e688556114405037c06ebf833e91e4ec805
rafidulislam19 <68073987+rafidulislam19@users.noreply.github.com>
2025-05-18 17:55:53 +0600

    student

--- student/urls.py
+++ student/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name="index"),
+    path('', views.student_list, name="student_list"),
+    path('add/', views.add_student, name="add_student"),
+    # path('', views.student_list, name="student_list"),
+    # path('', views.student_list, name="student_list"),
+    # path('', views.student_list, name="student_list"),
 ]
